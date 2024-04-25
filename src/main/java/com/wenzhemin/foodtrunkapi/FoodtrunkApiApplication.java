c0044e99fce8f27e21617fa0368ea6b118bc6dc0
Jason Wen <jason.wen.ext@louisvuitton.com>
2024-04-25 23:27:26 +0800

    initiate project.

---
+++ src/main/java/com/wenzhemin/foodtrunkapi/FoodtrunkApiApplication.java
@@ -0,0 +1,13 @@
+package com.wenzhemin.foodtrunkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtrunkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtrunkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wenzhemin/foodtrunkapi/FoodtrunkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenzhemin.foodtrunkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtrunkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
