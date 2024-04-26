ad8b1cf3692add42dc80a29bdf345fcf9ac344f3
Jason Wen <wenzhemintd@icloud.com>
2024-04-26 10:56:14 +0800

    create index controller.

---
+++ src/main/java/com/wenzhemin/foodtrunkapi/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.wenzhemin.foodtrunkapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "Hello and Welcome to the FoodTrunk application. You can get filtered FoodFacility info by making a GET request to /api/facilities endpoint.";
+    }
+}
